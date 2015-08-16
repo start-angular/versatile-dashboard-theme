907d21c5c1275790445f01ebfc3291b8529f180c
maximilianwollnik <maximilian@maximilianwollnik.de>
2015-08-16 17:47:22 +0200

    changes: own menu for private homepage
    - starting to add my own categories

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,8 +13,10 @@ var states = [
         { name: 'login', text: "Login", state: { visible: false, url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl' } },
         { name: 'dashboard', text: "Dashboard", state: { visible: false, url: '/dashboard', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'DashboardCtrl' } },
         { name: 'overview', text: "Overview", state: { visible: true, url: '/overview', parent: 'dashboard', templateUrl: 'views/dashboard/overview.html' } },
-        { name: 'reports', text: "Reports", state: { visible: true, url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html' } },
-        { name: 'logout', text: "Logout", state: { visible: true, url: '/login'} }
+        { name: 'reports', text: "Reports", state: { visible: false, url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html' } },
+        { name: 'resume', text: "Resume", state: { visible: true, url: '/resume', parent: 'dashboard', templateUrl: 'views/dashboard/resume.html' } },
+        { name: 'samples', text: "Samples", state: { visible: true, url: '/samples', parent: 'dashboard', templateUrl: 'views/dashboard/samples.html' } },
+        { name: 'contact', text: "Contact", state: { visible: true, url: '/contact', parent: 'dashboard', templateUrl: 'views/dashboard/contact.html' } }
     ];
    
 var yapp = angular.module('yapp', [
@@ -27,7 +29,7 @@ yapp.value('states', states);
 
 yapp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/dashboard');
     
     angular.forEach(states, function (state) {
         $stateProvider.state(state.name, state.state);
