346a263e243493aa64385647b320cfa4fad00e6b
Luiz Carlos Barreira <barreirinha@gmail.com>
2016-02-20 13:31:48 -0300

    HTML minify Module update
    minifyHtml is deprecated. Now we should use htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('html', ['styles'], function() {
     .pipe($.if('*.css', cssChannel()))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
 
